4c1b22378c7fa603fcbaa6b0f2752ee565682abb
Megaladon <stanko.demon@gmail.com>
2022-07-23 19:24:58 +0300

    Add FillArray

--- Project.cs
+++ Project.cs
@@ -8,3 +8,12 @@ string[] CreateArray(int count)
 {
     return new string[count];
 }
+
+void FillArray(string[] array)
+{
+    Console.WriteLine("Введите массив строк:");
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+}
